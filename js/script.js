e6dbbc17a15670865c43c726632e2c62c42b1228
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2021-12-11 15:23:15 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,5 +10,32 @@ input.addEventListener("keydown", function(event){
 
 function addItem(){
   var divParent = document.createElement("div");
-  var divParent = document.createElement("div");
+  var divChild = document.createElement("div");
+  var checkIcon = document.createElement("i");
+  var trashIcon = document.createElement("i");
+  
+  divParent.className = "item";
+  divParent.innerHTML = '<div>'+input.value+'</div>';
+  
+  ckeckIcon.className = "fas fa-check-square";
+  ckeckIcon.style.color = "lightgray";
+  ckeckIcon.addEventListener("click", function(){
+    ckeckIcon.style.color = "limegreen";
+  })
+  
+  divChild.appendChild(ckeckIcon);
+  
+  trashIcon.className = "fas fa-trash";
+  trashIcon.style.color = "darkgray";
+  trashIcon.addEventListener("click", function(){
+    divParent.remove();
+  })
+  
+  divChild.appendChild(trashIcon);
+  
+  divParent.appendChild(divChild);
+  
+  toDoItems.appendChild(divParent);
+  
+  input.value = '';
 }
