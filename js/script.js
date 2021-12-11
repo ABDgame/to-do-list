abb02ebb839cc4b96262183e694f127f7e119c21
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2021-12-11 13:43:15 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
+const toDoItems = document.getElementByClassName
+("to-do-items")[0];
+const input = document.getElementById("input");
+const trashIcon = document.getElementById("trash");
 
+input.addEventListener("keydown", function(event){
+  if(event.key === "Enter")
+     addItem();
+})
+
+function addItem(){
+  var divParent = document.createElement("div");
+  var divParent = document.createElement("div");
+}
