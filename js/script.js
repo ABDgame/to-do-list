22d25ab81d139fb749f04a5e50ebf3f215ac1f1b
Débora Inocêncio <30512134+MQJogos@users.noreply.github.com>
2021-12-14 15:56:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const toDoItems = document.getElementByClassName
+const toDoItems = document.getElementsByClassName
 ("to-do-items")[0];
 const input = document.getElementById("input");
 const trashIcon = document.getElementById("trash");
